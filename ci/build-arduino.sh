f42bfd494aa99bae0dcc7b212ea0e0f2d4f97cce
hkarthik1997 <karthik@zeddigital.net>
2022-02-10 17:24:23 +0530

    alignment6

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -13,11 +13,14 @@ export PATH=$PATH:$GITHUB_WORKSPACE/bin
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 arduino-cli config init
 arduino-cli core update-index
+arduino-cli core update-index --additional-urls http://arduino.esp8266.com/stable/package_esp8266com_index.json
+arduino-cli core update-index
+arduino-cli core install esp8266:esp8266
 # Install Arduino AVR core
 arduino-cli core install arduino:avr
 # Link Arduino library
 ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
 # Compile all *.ino files for the Arduino Uno
 for f in **/*.ino ; do
-    arduino-cli compile --fqbn esp8266:esp8266:d1 $f
+    arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 $f
 done
