05075bddabe383c8aa21de112ca1517606964e20
hkarthik1997 <karthik@zeddigital.net>
2022-02-10 17:09:48 +0530

    alignment6

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -15,11 +15,9 @@ arduino-cli config init
 arduino-cli core update-index
 # Install Arduino AVR core
 arduino-cli core install arduino:avr
-/opt/arduino/arduino --pref "boardsmanager.additional.urls=http://arduino.esp8266.com/stable/package_esp8266com_index.json" --save-prefs
-/opt/arduino/arduino --install-boards esp8266:esp8266 --save-prefs
 # Link Arduino library
 ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/CI_Test_Library
 # Compile all *.ino files for the Arduino Uno
 for f in **/*.ino ; do
-    /opt/arduino/arduino -v --verify --board esp8266:esp8266:nodemcu $f
+    arduino-cli compile --fqbn esp8266:esp8266:d1 $f
 done
